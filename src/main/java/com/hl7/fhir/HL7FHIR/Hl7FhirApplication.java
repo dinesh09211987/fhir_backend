163576a1d73dc19a779ba63e6ff0720db4f19d94
lamichhanes <santoshlamichhane04@outlook.com>
2020-12-12 19:28:12 -0800

    project setup initial commit

---
+++ src/main/java/com/hl7/fhir/HL7FHIR/Hl7FhirApplication.java
@@ -0,0 +1,13 @@
+package com.hl7.fhir.HL7FHIR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hl7FhirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hl7FhirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hl7/fhir/HL7FHIR/Hl7FhirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hl7.fhir.HL7FHIR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hl7FhirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
